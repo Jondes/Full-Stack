63ff2d35db3911a71a3de285996d5ba938cddb3a
Johnson E Guedes <jondes560@hotmail.com>
2022-06-21 07:48:55 +0100

    Create index.js

---
+++ react-mernstack-crud/backend/index.js
@@ -0,0 +1,36 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyparser = require('body-parser');
+
+// Express Route
+const studentRoute = require('../backend/routes/student.route');
+const bodyParser = require('body-parser');
+// Connecting mongoDB Database
+mongoose
+    .connect('mongodb://127.0.0.1:27017/mydatabase')
+    .then((x)=>{
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+})); 
+app.use(cors());
+app.use('/students', studentRoute)
+
+//PORT
+const port = process.env.PORT || 4000;
+const server = app.listen(port, ()=>{
+    console.log('Connected to port'+ port)
+})
+// 404 Error
+app.use((req, res, next)=>{
+    next(createError(404));
+});
+app.use(function(err, req, res, next){
+    console.error(err.message);
+    if(!err.statusCode) err.statusCode= 500;
+    res.status(err.statusCode).send(err.message);
+});
